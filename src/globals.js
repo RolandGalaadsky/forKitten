d86b72194fafa94b4492cf51b1f17d077012227b
RolandGalaadsky <oxxxyivan@gmail.com>
2018-01-01 19:28:50 +0200

    try do responsive

--- src/globals.js
+++ src/globals.js
@@ -1,6 +1,6 @@
 let windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 let windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 let snowNumbers = 200
-const TREEHEIGHT = 1;
+const TREEHEIGHT = 10;
 let all = document.getElementById("bd");
 let tree = document.getElementById("tree");
