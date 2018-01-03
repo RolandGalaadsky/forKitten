8767ce73ea280476dc91dbe48aec43ce48ae40a7
RolandGalaadsky <oxxxyivan@gmail.com>
2018-01-03 18:48:36 +0200

    not to fast

--- src/cat.js
+++ src/cat.js
@@ -1,22 +1,27 @@
 let direction = 1;
 let rotate = 9;
 let count = 0;
-
+let flag = false;
 
 cat.onclick = (event) => 
 {
-	moveTail();
-	let catInterval = setInterval(moveTail,625);
-	function moveTail()
+	if (!flag)
 	{
-		direction *= -1;
-		rotate *= -1;
-		tail.style.transform = "scaleY(" + direction + ")" + "rotate(" + rotate +"deg)";
-		count++;
-		if (count == 6)
+		flag = true;
+		moveTail();
+		let catInterval = setInterval(moveTail,625);
+		function moveTail()
 		{
-			count = 0;
-			clearInterval(catInterval);
+			direction *= -1;
+			rotate *= -1;
+			tail.style.transform = "scaleY(" + direction + ")" + "rotate(" + rotate +"deg)";
+			count++;
+			if (count == 6)
+			{
+				flag = false;
+				count = 0;
+				clearInterval(catInterval);
+			}
 		}
 	}
 }
